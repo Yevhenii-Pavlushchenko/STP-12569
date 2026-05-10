ea745fac8ebde06451a9368d4ca3b77a83343aa1
Artem Bagno <bgn.rtm@gmail.com>
2026-05-10 21:30:23 +0300

    solution mobile menu and small fix header itd

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,11 +1,20 @@
-const openBtnEl = document.querySelector('[data-action="open"]');
-const closeBtnEl = document.querySelector('[data-action="close"]');
-const burgerMenuEl = document.querySelector('[data-visible]');
+const openBtnEl = document.querySelector('[data-menu-open]');
+const closeBtnEl = document.querySelector('[data-menu-close]');
+const modalViewEl = document.querySelector('.mobile-menu');
+const navListEl = document.querySelector('.mm-list-navigation');
 
-openBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'open';
+openBtnEl.addEventListener('click', () => {
+  modalViewEl.classList.add('is-open');
+  closeBtnEl.addEventListener('click', handleClose);
+  document.body.classList.add('no-scroll');
 });
 
-closeBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'close';
+const handleClose = () => {
+  modalViewEl.classList.remove('is-open');
+  document.body.classList.remove('no-scroll');
+  closeBtnEl.removeEventListener('click', handleClose);
+};
+
+navListEl.addEventListener('click', e => {
+  if (e.target.tagName === 'A') handleClose();
 });
--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-// import './js/burgerMenu';
 import './js/how-to-play-swiper';
 import './js/features-swiper';
+import './js/burgerMenu';
